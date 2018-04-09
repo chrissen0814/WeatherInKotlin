139879bd633ffcc4cbb2d2937cf7675e7bd77d2a
chrissen <chrissen0814@gmail.com>
2018-04-09 18:17:55 +0800

    Initial commit

---
+++ app/src/main/java/com/chrissen/kotlindemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.chrissen.kotlindemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message.text = "Hello Kotlin!"
+    }
+}
---
+++ app/src/main/java/com/chrissen/kotlindemo/Teacher.kt
@@ -0,0 +1,13 @@
+package com.chrissen.kotlindemo
+
+/**
+ *  Function:
+ *  <br/>
+ *  Describe:
+ *  <br/>
+ *  Author: chris on 2018/4/9.
+ *  <br/>
+ *  Email: sunqirui@jiuhuar.com
+ */
+
+class Teacher(name : String , surname : String , age : Int) : Person(name , surname)
